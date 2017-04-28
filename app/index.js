e3b9c3cf27c21d55db461830d920c07f78baa0e0
Facundo Liston <facundoliston@MacBook-Pro-de-Facundo.local>
2017-04-28 13:03:46 -0300

    Actualziado

--- app/index.js
+++ app/index.js
@@ -15,16 +15,16 @@ const spotifyApi = new SpotifyWebApi({
 
 // Retrieve an access token
 /* eslint-disable*/
-spotifyApi.clientCredentialsGrant()
-  .then((data) => {
-    console.log('The access token expires in ' + data.body["expires_in"]);
-    console.log('The access token is ' + data.body['access_token']);
-
-    // Save the access token so that it's used in future calls
-    spotifyApi.setAccessToken(data.body['access_token']);
-  }, function(err) {
-    console.log('Something went wrong when retrieving an access token', err.message);
-  });
+// spotifyApi.clientCredentialsGrant()
+//   .then((data) => {
+//     console.log('The access token expires in ' + data.body["expires_in"]);
+//     console.log('The access token is ' + data.body['access_token']);
+//
+//     // Save the access token so that it's used in future calls
+//     spotifyApi.setAccessToken(data.body['access_token']);
+//   }, function(err) {
+//     console.log('Something went wrong when retrieving an access token', err.message);
+//   });
 
 app.use(metrics.middleware);
 
--- app/routes/index.js
+++ app/routes/index.js
@@ -7,12 +7,13 @@ const express = require('express');
 const router = express.Router();
 
 // TODO sacar esto a un modulo que no se porque no pude
-const clientId = '79c1fdf6a43243378e995732c34deb22';
-const clientSecret = '80da3e5874bf4673913a1475301011d7';
+const clientId = '30e1416683344493acfe11af2257e51c';
+const clientSecret = '17ababd4c6d7474190a5a5730cb6832e';
 const spotifyApi = new SpotifyWebApi({
   clientId,
   clientSecret,
 });
+
 
 // Ejemplo de traer un album probado con postman
 router.get('/', (req, res) => {
@@ -24,5 +25,46 @@ router.get('/', (req, res) => {
       console.log(err);
     });
 });
+
+// Retrieve an access token
+/* eslint-disable*/
+router.get('/token', (req, res) => {
+  spotifyApi.clientCredentialsGrant()
+    .then((data) => {
+      res.send(data.body['access_token']);
+      // console.log('The access token expires in ' + data.body["expires_in"]);
+      // console.log('The access token is ' + data.body['access_token']);
+
+      // Save the access token so that it's used in future calls
+      spotifyApi.setAccessToken(data.body['access_token']);
+    }, function(err) {
+      console.log('Something went wrong when retrieving an access token', err.message);
+    });
+});
+
+router.get('/playlist', (req, res) => {
+  user();
+  spotifyApi.getUserPlaylists('fliston22')
+    .then((data) => {
+      console.log('Retrieved playlists', data.body);
+      res.send('Retrieved playlists' + data.body);
+    }, function(err) {
+      console.log('Something went wrong!', err);
+    });
+});
+
+function user() {
+  spotifyApi.clientCredentialsGrant()
+    .then((data) => {
+      res.send(data.body['access_token']);
+      // console.log('The access token expires in ' + data.body["expires_in"]);
+      console.log('The access token is ' + data.body['access_token']);
+
+      // Save the access token so that it's used in future calls
+      spotifyApi.setAccessToken(data.body['access_token']);
+    }, function(err) {
+      console.log('Something went wrong when retrieving an access token', err.message);
+    });
+}
 
 module.exports = router;
