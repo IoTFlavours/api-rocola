1eaff5a32a1504068ca4c7f3e4ab2512df5c4ebd
Agustin <elmaco92@gmail.comm>
2017-04-30 13:15:41 -0300

    cosas nuevas qeu no habia pusheado

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,25 +7,16 @@ const SpotifyWebApi = require('spotify-web-api-node');
 const router = express.Router();
 
 // TODO arreglar lo del module!
-function spotifyApi() {
-  const clientId = '79c1fdf6a43243378e995732c34deb22';
-  const clientSecret = '80da3e5874bf4673913a1475301011d7';
-  const redirectUri = 'http://localhost:8081/auth/spotify/callback';
-  const scopes = ['user-read-private', 'user-read-email', 'playlist-modify-public', 'playlist-modify-private'];
+const clientId = '79c1fdf6a43243378e995732c34deb22';
+const clientSecret = '80da3e5874bf4673913a1475301011d7';
+const redirectUri = 'http://localhost:8081/auth/spotify/callback';
+const scopes = ['user-read-private', 'user-read-email', 'playlist-modify-public', 'playlist-modify-private'];
 
-  const spotify = new SpotifyWebApi({
-    clientId,
-    clientSecret,
-    redirectUri,
-  });
-
-  const authorizeURL = spotify.createAuthorizeURL(scopes);
-  console.log('--------------------------- COPY HERE authorizeURL-------------------------------');
-  console.log(authorizeURL);
-  console.log('--------------------------- COPY HERE authorizeURL-------------------------------');
-  return spotify;
-}
-const spotify = spotifyApi();
+const spotify = new SpotifyWebApi({
+  clientId,
+  clientSecret,
+  redirectUri,
+});
 
 // Ejemplo de traer un album probado con postman
 router.get('/', (req, res) => {
@@ -87,6 +78,12 @@ router.get('/user/:uid/playlist/:pid/addtrack/:tid/position/:p', (req, res) => {
     .catch((err) => {
       console.log(err);
     });
+});
+// SPOTIFY AUTHENTIFICATION
+router.get('/auth/spotify', (req, res) => {
+  console.log('hola');
+  const authorizeURL = spotify.createAuthorizeURL(scopes);
+  res.redirect(authorizeURL);
 });
 
 // SPOTIFY CALLBACK
